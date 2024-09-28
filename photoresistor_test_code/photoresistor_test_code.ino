e049c21ba01225dd2db54e13ab4bf8027042ceb2
ncwong26 <nathancwong26@gmail.com>
2024-09-28 10:48:31 -0600

    testing the tilt of the phototransistor

--- photoresistor_test_code/photoresistor_test_code.ino
+++ photoresistor_test_code/photoresistor_test_code.ino
@@ -1,12 +1,49 @@
-const int prPin = A0
+const int tPrPin = A0
+const int bPrPin = A1
+const int lPrPin = A2
+const int rPrPin = A3
 
 void setup() {
-  // initialize the baude rate
+  //initialize the baude rate
   Serial.begin(9600);
 }
 
 void loop() {
-  int val = 0;
-  val = analogRead(prPin);
-  Serial.println(val);
+  //initialize values for photoresistors
+  int tVal = 0;
+  int bVal = 0;
+  int lVal = 0;
+  int rVal = 0;
+
+  //read values from photoresistors
+  tVal = analogRead(tPrPin);
+  bVal = analogRead(bPrPin);
+  lVal = analogRead(lPrPin);
+  rVal = analogRead(rPrPin);
+
+  if(tVal < bVal)
+  {
+    Serial.println("down");
+  }
+  else if(bVal < tVal)
+  {
+    Serial.println("up");
+  }
+  else
+  {
+    Serial.println("pitch good");
+  }
+
+  if(lVal < rVal)
+  {
+    Serial.println("turn left");
+  }
+  else if(rVal < lVal)
+  {
+    Serial.println("turn right");
+  }
+  else
+  {
+    Serial.println("yaw good");
+  }
 }
